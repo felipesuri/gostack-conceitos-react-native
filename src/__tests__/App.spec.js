e269975dd20dbaae8879dca1f1b1e6d3c72b6a4a
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-19 09:35:51 -0300

    :recycle: modified code style

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,86 +1,81 @@
-import "react-native";
-import React from "react";
+import 'react-native'
+import React from 'react'
 
-import AxiosMock from "axios-mock-adapter";
-import api from "../services/api";
-const apiMock = new AxiosMock(api);
+import AxiosMock from 'axios-mock-adapter'
+import api from '../services/api'
+const apiMock = new AxiosMock(api)
 
-import App from "../App";
-import { render, fireEvent, act } from "@testing-library/react-native";
+import App from '../App'
+import { render, fireEvent, act } from '@testing-library/react-native'
 
 const wait = (amount = 0) => {
-  return new Promise((resolve) => setTimeout(resolve, amount));
-};
+  return new Promise(resolve => setTimeout(resolve, amount))
+}
 
 const actWait = async (amount = 0) => {
   await act(async () => {
-    await wait(amount);
-  });
-};
+    await wait(amount)
+  })
+}
 
-const repositoryId = 'd6e43105-a559-45b7-8fd7-53416b415741';
+const repositoryId = 'd6e43105-a559-45b7-8fd7-53416b415741'
 
-apiMock.onGet("repositories").reply(200, [
+apiMock.onGet('repositories').reply(200, [
   {
     id: repositoryId,
-    title: "Desafio React Native",
-    url: "https://github.com/josepholiveira",
-    techs: ["React Native", "Node.js"],
+    title: 'Desafio React Native',
+    url: 'https://github.com/josepholiveira',
+    techs: ['React Native', 'Node.js'],
     likes: 0,
   },
-]);
+])
 
-jest.mock(
-  "react-native/Libraries/Components/Touchable/TouchableOpacity.js",
-  () => {
-    const { TouchableHighlight } = require("react-native");
+jest.mock('react-native/Libraries/Components/Touchable/TouchableOpacity.js', () => {
+  const { TouchableHighlight } = require('react-native')
 
-    const MockTouchable = (props) => {
-      return <TouchableHighlight {...props} />;
-    };
-
-    MockTouchable.displayName = "TouchableOpacity";
-
-    return MockTouchable;
+  const MockTouchable = props => {
+    return <TouchableHighlight {...props} />
   }
-);
 
-describe("Likes", () => {
-  it("should add a like to the like counter of the repository", async () => {
-    const { getByTestId } = render(<App />);
+  MockTouchable.displayName = 'TouchableOpacity'
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 1,
-      });
+  return MockTouchable
+})
 
-    await actWait();
+describe('Likes', () => {
+  it('should add a like to the like counter of the repository', async () => {
+    const { getByTestId } = render(<App />)
 
-    fireEvent.press(getByTestId(`like-button-${repositoryId}`));
+    apiMock.onPost(`repositories/${repositoryId}/like`).reply(200, {
+      id: repositoryId,
+      title: 'Desafio React Native',
+      url: 'https://github.com/josepholiveira',
+      techs: ['React Native', 'Node.js'],
+      likes: 1,
+    })
 
-    await actWait();
+    await actWait()
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("1 curtida");
+    fireEvent.press(getByTestId(`like-button-${repositoryId}`))
 
-    apiMock
-      .onPost(`repositories/${repositoryId}/like`)
-      .reply(200, {
-        id: repositoryId,
-        title: "Desafio React Native",
-        url: "https://github.com/josepholiveira",
-        techs: ["React Native", "Node.js"],
-        likes: 2,
-      });
+    await actWait()
 
-    fireEvent.press(getByTestId(`like-button-${repositoryId}`));
+    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent('1 curtida')
 
-    await actWait();
+    apiMock.onPost(`repositories/${repositoryId}/like`).reply(200, {
+      id: repositoryId,
+      title: 'Desafio React Native',
+      url: 'https://github.com/josepholiveira',
+      techs: ['React Native', 'Node.js'],
+      likes: 2,
+    })
 
-    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent("2 curtidas");
-  });
-});
+    fireEvent.press(getByTestId(`like-button-${repositoryId}`))
+
+    await actWait()
+
+    expect(getByTestId(`repository-likes-${repositoryId}`)).toHaveTextContent(
+      '2 curtidas'
+    )
+  })
+})
